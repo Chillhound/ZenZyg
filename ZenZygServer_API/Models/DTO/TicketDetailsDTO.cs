b3781e90172b9eef89893f88dac6caeddefe8ad5
Mikkel Møller Jensen <momj@itu.dk>
2020-12-01 00:34:52 +0100

    api build

---
+++ ZenZygServer_API/Models/DTO/TicketDetailsDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZenZygServer_API.Models.DTO
+{
+    public class TicketDetailsDTO
+    {
+        public int TicketId { get; set; }
+        [Required]
+        public int? StoreId { get; set; }
+        [Required]
+        public string QRData { get; set; }
+        [Required]
+        public int? CustomerId { get; set; }
+      
+    }
+}
